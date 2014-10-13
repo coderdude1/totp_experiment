1259a507616761c64322e854db544ae0332c75ae
Randy <Randy@dood>
2014-10-13 09:59:34 -0500

    Got the webapp working, ie the spring context is loading now

---
+++ WebApp/readme.md
@@ -0,0 +1,6 @@
+Simple spring MVC project, derived from the intellij tutorial found here:
+
+http://confluence.jetbrains.com/display/IntelliJIDEA/Getting+Started+with+Spring+MVC,+Hibernate+and+JSON
+
+Its slowly evolving to a nice playground for experimenting with jquery, spring security, hibernate, etc.  I've migrated
+the db stuff from H2 to postgres, added jquery 1.something, etc.
--- WebApp/src/main/java/com/dood/web/controller/UserController.java
+++ WebApp/src/main/java/com/dood/web/controller/UserController.java
@@ -1,7 +1,17 @@
 package com.dood.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/")
 public class UserController {
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
+
+    public String someDefaultHandler(ModelMap model) {
+        return "users";
+    }
 }
