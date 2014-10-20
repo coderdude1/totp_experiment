443817f1946f087a926987f17d526cc0c6b8f2ba
Randy <drbugsmn@gmail.com>
2014-10-19 20:26:42 -0500

    got spring security wired in, java config, removed web.xml woot!

---
+++ WebApp/src/main/java/com/dood/web/config/MessageSecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package com.dood.web.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by This should load the security chain (seems to function the same as a filter?
+ */
+public class MessageSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
