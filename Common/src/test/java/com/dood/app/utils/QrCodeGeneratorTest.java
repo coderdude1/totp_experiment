4750dd391f7c797fd9d203f00ca64e5ecb27730b
Randy <drbugsmn@gmail.com>
2014-11-08 15:01:00 -0600

    Added QR Code generation for loading the secret into android Google Auth

---
+++ Common/src/test/java/com/dood/app/utils/QrCodeGeneratorTest.java
@@ -0,0 +1,16 @@
+package com.dood.app.utils;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+
+public class QrCodeGeneratorTest extends TestCase {
+
+    public void testGenerateQrCode() throws Exception {
+        File qrTempFile = File.createTempFile("qr_test", ".png");
+        QrCodeGenerator qrCodeGenerator = new QrCodeGenerator();
+        qrCodeGenerator.generateTwoFactorAuthQrCode("rwHost", "NBW7TMRLXGTMXRMC", qrTempFile);
+
+        //opened the qr code with my phone, it loaded into GoogleAuthenticator just fine
+    }
+}
